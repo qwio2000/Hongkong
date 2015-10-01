1e935f874a4c0b5a004c58d7608b070da6617c53
devillee147 <devillee147@naver.com>
2015-10-01 17:24:38 +0900

    판촉물

---
+++ src/main/java/com/jeiglobal/hk/controller/promoitem/PromotionItemController.java
@@ -0,0 +1,37 @@
+package com.jeiglobal.hk.controller.promoitem;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.jeiglobal.hk.domain.auth.LoginInfo;
+
+
+/**
+ * @since  2015-10-01
+ * @author 이지은
+ *
+ */
+
+@Slf4j
+@Controller
+public class PromotionItemController {
+	
+	//페이징 범위
+	private static final int PAGE_BLOCK_SIZE = 10;
+	//한 페이지에 출력할 게시물 개수
+	private static final int PAGE_SIZE = 10;
+	
+	
+	@RequestMapping(value={"/ja/promoitem/promoitemlist","/ja/promoitem"}, method={RequestMethod.GET,RequestMethod.HEAD})
+	public String List(@ModelAttribute LoginInfo loginInfo) {
+		String userType = loginInfo.getUserType();
+		log.debug("Getting List Page, UserType : {}", userType);
+		
+		return "promotionitem/list";
+	}
+
+}
---
+++ src/main/resources/templates/promotionitem/list.ftl
@@ -0,0 +1,12 @@
+<#include "/include/header.ftl">
+<!-- Main Content -->
+<div class="content">
+	<h2 class="conTit">PromotionItem List</h2>
+	<form id="PromotionItem" action="/ja/promoitem/promoitemlist" method="get">		
+		<ul class="memSearch" <#if loginInfo.userType == "JA"> style="float:right;"</#if>>
+			<a href="#">글쓰기</a>
+		</ul>
+	</form>
+</div>
+<!--// Main Content -->
+<#include "/include/footer.ftl">
